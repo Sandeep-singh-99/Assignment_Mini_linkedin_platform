d0959abda9d57207906940cf2d6fd5c132b6e847
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-08-03 15:10:15 +0530

    feat: configure CORS to allow requests from localhost:5173 with credentials

--- server/server.js
+++ server/server.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use(cookieParser());
 
 app.use('/api/v1/auth', authRoutes);
